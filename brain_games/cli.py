71db444de623c4646441eb4fdace78551e28760f
mareedez <xxl_century@yahoo.com>
2021-05-07 00:41:17 -0700

    flake8 added

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,13 @@
+"""Welcoming message."""
+
 import prompt
 
 
 def welcome_user():
+    """Says hello.
+
+    Returns:
+        greeting user by name
+    """
     name = prompt.string('May I have your name? ')
-    return "Hello,", name
-
-
+    return 'Hello,', name
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
+"""Welcoming message."""
+
 from brain_games.cli import welcome_user
 
 
 def main():
-    print("Welcome to the Brain Games!")
+    """Take user's name and greet him."""
+    print('Welcome to the Brain Games!')
     welcome_user()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
