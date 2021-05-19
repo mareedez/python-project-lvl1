8e7e0e4d5b24ca135be0f4454af362c19e06b01b
mareedez <xxl_century@yahoo.com>
2021-05-19 14:52:51 -0700

    added prime game

---
+++ brain_games/games/game_prime.py
@@ -0,0 +1,50 @@
+"""Brain-prime logic."""
+from brain_games import game_core
+from sympy import isprime
+
+
+def question():
+    """Generate random number.
+
+    Returns:
+        number: integer in range from 1 to 100
+    """
+    return game_core.generate_random_number()
+
+
+def is_prime(number):
+    """Check if number is prime.
+
+    Args:
+        number: integer number
+
+    Returns:
+        'yes' if number is prime else 'no'
+    """
+    if isprime(number):
+        return 'yes'
+    return 'no'
+
+
+def rules():
+    """Prime-game rules description.
+
+    Returns:
+        gameplay guidance
+    """
+    return 'Answer "yes" if given number is prime. Otherwise answer "no".\n'
+
+
+def get_answers():
+    """Get answer from user and calculate correct answer.
+
+    Returns:
+        correct_answer: expected answer
+        user_answer: answer entered by the user
+    """
+    number = question()
+    correct_answer = is_prime(number)
+    quest = 'Question: {0} '
+    user_answer = input(quest.format(number))
+    print('Your answer:', user_answer)
+    return correct_answer, user_answer
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+"""Brain-prime script."""
+from brain_games import game_core
+from brain_games.games import game_prime
+
+
+def main():
+    """Run brain-even game."""
+    game_core.play_the_game(game_prime)
+
+
+if __name__ == '__main__':
+    main()
