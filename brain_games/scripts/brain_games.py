fd0d24bc68fe8742be0c552163fb5121125498b1
mareedez <xxl_century@yahoo.com>
2021-05-19 23:24:15 -0700

    restore brain_games file

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Test module."""
+
+
+def main():
+    """Brain-game main function.
+
+    Returns:
+        greeting
+    """
+    return 'Welcome!'
+
+
+if __name__ == '__main__':
+    main()
