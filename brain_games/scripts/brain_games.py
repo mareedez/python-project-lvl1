08617447d85e74961f0955d378ad4ef672f318e1
mareedez <xxl_century@yahoo.com>
2021-05-20 00:04:33 -0700

    fix linter issue

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 """Test module."""
+import prompt
+
+
+def welcome_user():
+    """Ask for username and say Hello."""
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print('Hello, {0}!'.format(user_name))
 
 
 def main():
     """Brain-game main function."""
-    print('Welcome to the Brain Games!')
+    welcome_user()
 
 
 if __name__ == '__main__':
